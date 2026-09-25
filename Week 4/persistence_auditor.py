254bedb9b536e89662aadafd70a95f1a2298c779
SIT-PhoneMyat <2604706@sit.singaporetech.edu.sg>
2026-09-25 13:36:44 +0800

    Week 4 - Implement load inventory

--- Week 4/persistence_auditor.py
+++ Week 4/persistence_auditor.py
@@ -4,7 +4,15 @@ path = Path("Week 4/inventory.txt") # idk why adding "Week 4" in the path can ma
 print("Path: ", path)
 
 def load_inventory():
-    pass
+    if path.exists():   # if file exists
+        print("Current Orders: \n")
+        with path.open("r") as file:
+            for line in file:
+                print(line.strip())
+            else:
+                return int(line[:4])    # if orders exist, return last order id
+    else:
+        return None # if empty, the order id will start at 1001
 
 def save_inventory():
     pass
