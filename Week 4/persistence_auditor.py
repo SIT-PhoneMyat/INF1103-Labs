7508cde2450c87f3c677869b62eb015af200dfd0
SIT-PhoneMyat <2604706@sit.singaporetech.edu.sg>
2026-09-25 13:59:00 +0800

    Week 4 - Fix bugs and finalize the program

--- Week 4/persistence_auditor.py
+++ Week 4/persistence_auditor.py
@@ -6,12 +6,15 @@ print("Path: ", path)
 def load_inventory():
     if path.exists():   # if file exists
         print("Current Orders: \n")
+        order_id = 1000 # if file exists but empty, the order id will start at 1001
         with path.open("r") as file:
             for line in file:
-                print(line.strip())
-            else:
-                return int(line[:4])    # if orders exist, return last order id
+                if line.strip() != "": # we are skipping empty lines
+                    print(line.strip()) 
+                    order_id = int(line[:4])  # Update order_id with the last seen order ID
+        return order_id
     else:
+        print("Current Orders: \n")
         return None # if empty, the order id will start at 1001
 
 def save_inventory(order):
@@ -24,14 +27,16 @@ last_order_id = load_inventory()
 while True:
     new_order_id = last_order_id + 1 if last_order_id is not None else 1001
     # print(f"\nNew Order ID: {new_order_id}")
+    print("\n" + "-"*20)
     product_name = input("Enter product name: ")
     quantity = int(input("Enter quantity: "))
     order = f"{new_order_id}, {product_name}, {quantity}"
     save_inventory(order)
 
-    print("New Order Added:")
+    print("\nNew Order Added:")
     print(order)
     print("\nOrder successfully saved to orders.txt")
+    print("-"*20)
 
     cmd = input("\nDo you want to add more orders? (Y/n) Type quit/q/n to exit the program: ")
     if cmd.lower() == "quit" or cmd.lower() == 'n' or cmd.lower() == 'q':
