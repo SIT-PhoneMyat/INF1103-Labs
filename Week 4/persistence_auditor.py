4f303998b13f98b82ce4fd07cb7fb43e3bc14b98
SIT-PhoneMyat <2604706@sit.singaporetech.edu.sg>
2026-09-25 13:39:06 +0800

    Week 4 - Track history and save new order

--- Week 4/persistence_auditor.py
+++ Week 4/persistence_auditor.py
@@ -22,10 +22,20 @@ last_order_id = load_inventory()
 # print("\nLast Order ID:", last_order_id)
 
 while True:
+    new_order_id = last_order_id + 1 if last_order_id is not None else 1001
+    # print(f"\nNew Order ID: {new_order_id}")
     product_name = input("Enter product name: ")
     quantity = int(input("Enter quantity: "))
+    order = f"{new_order_id}, {product_name}, {quantity}"
+    save_inventory(order)
+
+    print("New Order Added:")
+    print(order)
+    print("\nOrder successfully saved to orders.txt")
 
     cmd = input("\nDo you want to add more orders? (Y/n) Type quit/q/n to exit the program: ")
     if cmd.lower() == "quit" or cmd.lower() == 'n' or cmd.lower() == 'q':
         break
+    
+    last_order_id = new_order_id # item is added already, so the last order id is updated to the new order id
 
